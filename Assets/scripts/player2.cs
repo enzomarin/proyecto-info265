02d7b029e2dd4868b913f75b4ff8040a3331c6b7
enzomarin <enzomarin12@gmail.com>
2020-03-20 23:21:40 -0300

    arregle algunas animaciones del pj2

--- Assets/scripts/player2.cs
+++ Assets/scripts/player2.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class player2 : MonoBehaviour {
 
 	public float moveSpeed = 3.0f; // velocidad
+	public float moveSpeedRet = 1.5f;
 	public float jumpSpeed = 8.0f; // salto, cambia esta variable para saltar mas alto
 	public float gravity = 20.0f; // salto, cambia esta variable para saltar mas alto
 	public float rotationSpeed = 200.0f; //Rotacion camera sensibilidad
@@ -17,29 +18,71 @@ public class player2 : MonoBehaviour {
 	}
 
 	void Update () {
-		bool correr= Input.GetKey(KeyCode.I);
+		bool escudo = Input.GetKey(KeyCode.U);
 	// movimiento del personaje
+		bool correr = Input.GetKey(KeyCode.I);
+		bool retroceder = Input.GetKey(KeyCode.K);
+		bool girDer = Input.GetKey(KeyCode.J);
+		bool girIzq = Input.GetKey(KeyCode.L);
+
+		anim.SetBool("ret",retroceder);
 		anim.SetBool("run",correr);
+		anim.SetBool("girarDer",girDer);
+		anim.SetBool("girarIzq",girIzq);
+		if(retroceder){
+			moveSpeed = moveSpeedRet;
+		}
+		else
+		{
+			if(!escudo){
+				moveSpeed = 5.0f;
+			}
+			
+		}
+
 		x = Input.GetAxis("Horizontal2");
 		y = Input.GetAxis("Vertical2");
-		transform.Rotate(0, x*Time.deltaTime * rotationSpeed,0);
-		transform.Translate(0,0, y* Time.deltaTime * moveSpeed);
+		if(!escudo){
+			transform.Translate(0,0, y* Time.deltaTime * moveSpeed);
+			transform.Rotate(0, x*Time.deltaTime * rotationSpeed,0);
+		}
 
-	//-------------------------------------
+		
 
-	// ataque
+	//-----------------------------------------------------------------------------
+
+	// ------------------------------  Ataque  ---------------------------------------
 		bool atacar = Input.GetKeyDown(KeyCode.H);
+		bool ataqueEspecial = Input.GetKeyDown(KeyCode.Y);
 		if (atacar)
-		{
+		{	
+			
 			ataque();
 			anim.ResetTrigger("Atacar");
 			anim.SetTrigger("Atacar");
 		}
-	//---------------------------
+		if(ataqueEspecial){
+			// generar funcion atEspecial()
+			anim.ResetTrigger("AtaqueEsp");
+			anim.SetTrigger("AtaqueEsp");
+		}
+	//--------------------------------------------------------------------------------
 
 
+
+	// protegerse
+		
+		anim.SetBool("escudoIde",escudo);
+		if(escudo && !correr){
+			moveSpeed = 2f;
+		}
+
+
+	//----------------------------------------------------------------------------
+	
 	}
-	void ataque(){
 
+	void ataque(){
+		// codigo necesario para hacer daño etc.
 	}
 }
