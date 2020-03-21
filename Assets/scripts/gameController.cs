2ffbf6dc2c1312fae47cd48153b9960290b080bb
enzomarin <enzomarin12@gmail.com>
2020-03-21 20:21:36 -0300

    menu pausa2 (simple)

--- Assets/scripts/gameController.cs
+++ Assets/scripts/gameController.cs
@@ -6,8 +6,19 @@ using UnityEngine;
 
 public class gameController : MonoBehaviour
 {
+    public static gameController Instance;
     public Canvas hud;
     public Canvas canvPause;
+    void Awake()
+    {
+
+        //ALMACENA INSTANCIA
+        //Cualquier script puede usar este gamemanager usando gameController.Instance
+        if (Instance != null)
+            Destroy(Instance);
+        Instance = this;
+
+    }
     
     // Start is called before the first frame update
     void Start()
